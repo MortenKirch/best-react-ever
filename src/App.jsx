2a89f7776c71bfd2527028aa94b221b6dd66d487
MortenKirch <morten-kris@live.dk>
2024-09-06 11:02:16 +0200

    trejde forsøg

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import { Route, Routes } from 'react-router-dom';
-import HomePage from './pages/homePage'
+import HomePage from './pages/HomePage'
 import CreatePage from './pages/CreatePage';
 import Navbar from './components/Navbar';
 import UserDetailPage from "./pages/UserDetailPage"
