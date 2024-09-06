ac7bbd648884d716b5cb90af7a4fea4db1bb3d18
MortenKirch <morten-kris@live.dk>
2024-09-06 10:53:09 +0200

    first forsøg til pages

--- src/main.jsx
+++ src/main.jsx
@@ -4,25 +4,11 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc"; // this on might be "import react from "@vitejs/plugin-react";" in your solution
 
-// https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-    const config = {
-        plugins: [react()],
-        base: "/"
-    };
-
-    if (command !== "serve") {
-        config.base = "/react-vite-rest-post-app-with-auth/";
-    }
-
-    return config;
-});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.DEV ? "/" : "/best-react-ever/"}>
     <App />
     </BrowserRouter>
   </StrictMode>,
